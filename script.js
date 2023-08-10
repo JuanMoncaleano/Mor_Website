8c1b60e52b1816620b8648815a7af67ef54f5858
JuanMoncaleano <juankamilo992201@gmail.com>
2023-08-10 18:48:21 -0400

    Working on it

--- script.js
+++ script.js
@@ -8,12 +8,47 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 // Define the cities, coordinates, and images
 var cities = [
   {
-    name: "Toronto",
+    name: "CN_Tower",
     coordinates: [43.6426, -79.3871],
     content:
-      '<a href="toronto.jpg" target="_blank"><img src="toronto.jpg" width="200px"></a>',
+      '<a href="cn_tower.jpg" target="_blank"><img src="Pics/cn_tower.jpg" width="200px"></a>',
   },
-
+  {
+    name: "Blue_Mountain",
+    coordinates: [44.5011, -80.3161],
+    content:
+      '<a href="Blue_Mountain.jpg" target="_blank"><img src="Pics/Blue_Mountain.jpg" width="200px"></a>',
+  },
+  {
+    name: "Bogota",
+    coordinates: [4.711, -74.0721],
+    content:
+      '<a href="Bogota.jpg" target="_blank"><img src="Pics/Bogota.jpg" width="200px"></a>',
+  },
+  {
+    name: "Butterfly",
+    coordinates: [43.813, -79.3198],
+    content:
+      '<a href="Butterfly.jpg" target="_blank"><img src="Pics/Butterfly.jpg" width="200px"></a>',
+  },
+  {
+    name: "Cartagena",
+    coordinates: [10.3932, -75.4832],
+    content:
+      '<a href="Cartagena.jpg" target="_blank"><img src="Pics/Catagena.jpg" width="200px"></a>',
+  },
+  {
+    name: "Grand Bend",
+    coordinates: [43.313, -81.7562],
+    content:
+      '<a href="Grand_Bend.jpg" target="_blank"><img src="Pics/Grand_Bend.jpg" width="200px"></a>',
+  },
+  {
+    name: "Hamilton",
+    coordinates: [43.2557, -79.8711],
+    content:
+      '<a href="Hamilton.jpg" target="_blank"><img src="Pics/Hamilton.jpg" width="200px"></a>',
+  },
   // Add more cities here...
 ];
 
