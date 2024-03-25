0117812a1d6df0497a831c3f37d5e43a1f3f1d64
JuanMoncaleano <juanmoncaleano@Juans-MacBook-Pro.local>
2024-03-25 16:26:41 -0400

    changing locations

--- static/script.js
+++ static/script.js
@@ -84,7 +84,7 @@ const cities = [
   },
   {
     name: "Volca Arenal",
-    coordinates: [10.46333, -84.70333],
+    coordinates: [10.16333, -84.00333],
     content: createCityContent("Fortuna"),
   },
   {
@@ -94,7 +94,7 @@ const cities = [
   },
   {
     name: "Puente colgante",
-    coordinates: [10.461, -84.705],
+    coordinates: [10.961, -84.905],
     content: createCityContent("Bridge"),
   },
   {
